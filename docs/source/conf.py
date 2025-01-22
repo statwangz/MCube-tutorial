db2998dbe72189b5cf7544465ebeee94981b47ba
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2025-01-22 10:43:29 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MCube'
+copyright = '2025, Zhiwei Wang'
+author = 'Zhiwei Wang'
 
 release = '0.1'
 version = '0.1.0'
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
